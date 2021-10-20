4e296871a2f7b19992ed1aa20e5c612a3ef3d35b
Anik Das <anikdas0811@gmail.com>
2021-10-20 15:26:38 +0530

    feat: working ps prototype
    with pid, ppid, name and state
    
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+# Implementing the `ps` command
---
+++ src/ps.c
@@ -0,0 +1,108 @@
+#include <ctype.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define TRUE 1
+#define FALSE 0
+#define BUF_SIZE 1000
+#define num long long int
+
+struct proc_display_details {
+    num pid;
+    char tcomm[PATH_MAX];
+    char state;
+    num ppid;
+};
+
+// helper functions to read a single unit at a time
+void
+readone(num* x, FILE* input)
+{
+    fscanf(input, "%lld ", x);
+}
+void
+readstr(char* x, FILE* input)
+{
+    fscanf(input, "%s ", x);
+}
+void
+readchar(char* x, FILE* input)
+{
+    fscanf(input, "%c ", x);
+}
+
+// check if /proc/[id] id is a number, then its a process
+int
+is_process(char* str)
+{
+    int j = 0;
+    while (str[j] != '\0') {
+        if (!isdigit(str[j])) {
+            return FALSE;
+        }
+        j++;
+    }
+    return TRUE;
+}
+
+
+// string slice: https://stackoverflow.com/a/63990303/13162096
+// pretty table print process details
+void
+print_proc_details(struct proc_display_details proc)
+{
+    size_t len = strlen(proc.tcomm);
+    char clean_name[len];
+	memset(clean_name, '\0', sizeof(clean_name));
+    strncpy(clean_name, proc.tcomm + (size_t)1, len - 2);
+    printf("%20s | %6llu | %6llu | %5c \n", clean_name, proc.pid, proc.ppid, proc.state);
+}
+
+int
+main()
+{
+    num pid;
+    char tcomm[PATH_MAX];
+    char state;
+
+    num ppid;
+
+    DIR* procdir;
+    struct dirent* process;
+    procdir = opendir("/proc");
+    if (procdir == NULL) {
+        perror("opendir");
+        return 1;
+    }
+    char stat_path[BUF_SIZE];
+    FILE* fp;
+    // process /proc/[pid]/stat details
+    // https://man7.org/linux/man-pages/man5/proc.5.html
+    printf("%20s | %6s | %6s | %5s \n", "PROC", "PID", "PPID", "STATE");
+    while ((process = readdir(procdir)) != NULL) {
+        if (is_process(process->d_name)) {
+            strcpy(stat_path, "/proc/");
+            strcat(stat_path, process->d_name);
+            strcat(stat_path, "/stat");
+
+            fp = fopen(stat_path, "r");
+            if (fp == NULL) {
+                perror("fopen");
+            }
+
+            // printf("stat_path: %s\n", stat_path);
+            struct proc_display_details read_proc;
+            readone(&(read_proc.pid), fp);
+            readstr(read_proc.tcomm, fp);
+            readchar(&(read_proc.state), fp);
+            readone(&(read_proc.ppid), fp);
+            fclose(fp);
+            print_proc_details(read_proc);
+        }
+    }
+    closedir(procdir);
+}
